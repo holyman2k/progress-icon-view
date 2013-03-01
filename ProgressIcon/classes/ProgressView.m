de11256a2392865ffe8836e322347e69806af5e3
Charlie Wu <charlie.y.wu@gmail.com>
2013-03-01 15:52:03 +1100

    fix drawing full cycle leaves a line behind

--- ProgressIcon/classes/ProgressView.m
+++ ProgressIcon/classes/ProgressView.m
@@ -87,7 +87,7 @@
     CGFloat centerY = self.frame.size.height / 2;
     CGFloat radius = (self.frame.size.width > self.frame.size.height ? self.frame.size.height : self.frame.size.width) / 2 * PaddingPercentage;
     CGFloat startAngle = M_PI * 1.5;
-    CGFloat endAngle = (self.percentage * 2 + 1.5) * M_PI;
+    CGFloat endAngle = (self.percentage * 2 + 1.5 + .1) * M_PI;
 //    NSLog(@"x: %f, y: %f, start angle: %f, end angle: %f", centerX, centerY, startAngle, endAngle);
     
     CGContextRef context = UIGraphicsGetCurrentContext();
@@ -98,15 +98,6 @@
     
     CGContextBeginPath(context);
     
-    if (self.mode == STROKE_FILL_MODE){
-        CGContextSaveGState(context);
-        CGContextAddArc(context, centerX, centerY, radius, M_PI, 3 * M_PI, 0);
-        CGContextSetStrokeColorWithColor(context, self.progressColor.CGColor);
-        CGContextSetLineWidth(context, self.linkThickness);
-        CGContextStrokePath(context);
-        CGContextRestoreGState(context);
-    }
-    
     if (self.mode == FILL_MODE || self.mode == STROKE_FILL_MODE) {
         CGContextMoveToPoint(context, centerX, centerY);
         CGContextAddLineToPoint(context, centerX, centerY - radius);
@@ -115,15 +106,24 @@
     if (self.mode == STROKE_MODE) {
         CGContextSetLineWidth(context, self.linkThickness);
     } else {
-        CGContextSetLineWidth(context, .5);
+        CGContextSetLineWidth(context, 0.5);
     }
-    if (self.mode == STROKE_MODE) CGContextSetStrokeColorWithColor(context, self.progressColor.CGColor);
-    if (self.mode == FILL_MODE || self.mode == STROKE_FILL_MODE) CGContextSetFillColorWithColor(context, self.progressColor.CGColor);
+    CGContextSetStrokeColorWithColor(context, self.progressColor.CGColor);
+    CGContextSetFillColorWithColor(context, self.progressColor.CGColor);
     
     if (self.mode == FILL_MODE || self.mode == STROKE_FILL_MODE) {
         CGContextDrawPath(context, kCGPathFillStroke);   
     } else {
         CGContextStrokePath(context);
+    }
+    
+    if (self.mode == STROKE_FILL_MODE){
+        CGContextSaveGState(context);
+        CGContextAddArc(context, centerX, centerY, radius, 0, 2 * M_PI, 1);
+        CGContextSetStrokeColorWithColor(context, self.progressColor.CGColor);
+        CGContextSetLineWidth(context, self.linkThickness);
+        CGContextStrokePath(context);
+        CGContextRestoreGState(context);
     }
 }
 
